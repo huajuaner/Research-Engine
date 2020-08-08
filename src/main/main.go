c05bb66a3f03056669d668137b38d6325c4f5b9b
zzyhuajuan <zzyhuajuan@gmail.com>
2020-08-08 23:53:32 +0800

    crawler almost ready

--- src/main/main.go
+++ src/main/main.go
@@ -5,29 +5,49 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"log"
 	"net/http"
+	"net/url"
 	"regexp"
 	"sync"
 )
 
-func UrlValidate (url string )(Is bool ) {
-	if url != "" {
-		Is ,_ = regexp.MatchString(`(https?)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]`,url)
+type Node struct {
+	IfValidate bool
+	title string
+	author string
+	publisher string
+	abstract string
+}
+type typeSafeSaver struct {
+	mmp map [string] Node
+	mux sync.Mutex
+}
+
+var (
+	WebLimit = make (chan bool , 100 )
+	SafeSaver =typeSafeSaver{mmp:make (map[string] Node)}
+)
+
+func UrlValidate (uri string )(Is bool ) {
+	if uri != "" {
+		Is ,_ = regexp.MatchString(`^(https?)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]`,uri)
+		if Is {
+			Is ,_ = regexp.MatchString(`hitsz.edu.cn`,uri)
+		}
 		return
 	}
 	Is = false
 	return
 }
 // https://book.douban.com/subject/3912973/
-func BookValidate1(url string) (Is bool) {
-	if url != "" {
-		Is, _ = regexp.MatchString(`https://book.douban.com/subject/[\d]+/$`, url)
+func BookValidate1(uri string) (Is bool) {
+	if uri != "" {
+		Is, _ = regexp.MatchString(`http://www.hitsz.edu.cn/article/view/id-[\d]+.html$`, uri)
 		return
 	}
 	Is = false
 	return
 }
 
-var WebLimit = make (chan bool , 10 )
 
 
 func MyCrawler(dst string, depth int, wg *sync.WaitGroup) {
@@ -37,57 +57,77 @@ func MyCrawler(dst string, depth int, wg *sync.WaitGroup) {
 		<-WebLimit
 	}()
 	defer wg.Done()
+
+	SafeSaver.mux.Lock()
+	if _,ok := SafeSaver.mmp[dst];ok {
+		defer SafeSaver.mux.Unlock()
+		return
+	}
+	SafeSaver.mmp[dst] = Node{
+		IfValidate: true,
+	}
+	SafeSaver.mux.Unlock()
+
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", dst, nil)
+
+	req, err := http.NewRequest("GET", dst, nil)
 	req.Header.Set("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)")
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		log.Println(dst)
+		return
 	}
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 
 	doc.Find("a").Each(func(i int, sel *goquery.Selection) {
-		url, If_exist := sel.Attr("href")
-		if UrlValidate(url) == false {
+		uri, If_exist := sel.Attr("href")
+		if UrlValidate(uri) == false {
+			NewUrl,err := url.Parse(uri)
+			if err != nil {
+				return
+			}
+			BaseUrl,err := url.Parse(dst)
+			if err != nil {
+				return
+			}
+			uri = BaseUrl.ResolveReference(NewUrl).String()
+		}
+		if UrlValidate(uri) == false {
 			return
 		}
 		if If_exist == true && depth <= 2 {
 			wg.Add(1)
-			fmt.Println(url)
-			go MyCrawler(url,depth+1,wg)
+			go MyCrawler(uri,depth+1,wg)
 		}
-		//if If_exist == true && BookValidate1(url) {
-		//	fmt.Println(url)
-		//}
 	})
 	if BookValidate1(dst) == false {
 		return
 	}
 	// Find the review items
-	//doc.Find("div.info").Each(func(i int, s *goquery.Selection) {
-	//	// For each item found, get the band and title
-	//	url, IfExist := s.Find("div.title a").Attr("href")
-	//	if IfExist == true {
-	//		title := s.Find("a").Text()
-	//		author := s.Find("div.author").Text()
-	//		fmt.Println(i, title, author, url)
-	//	}
-	//})
+	doc.Find("div.detail_out").Each(func(i int, s *goquery.Selection) {
+		title := s.Find("div.title").Text()
+		fmt.Println(dst)
+		fmt.Println(title)
+	})
 	return
 }
 
 func main() {
 	var wg sync.WaitGroup
 	wg.Add( 1 )
-	go MyCrawler("https://book.douban.com/top250?icn=index-book250-all",0,&wg )
+	go MyCrawler("http://www.hitsz.edu.cn/article/index.html",0,&wg)
+	//go MyCrawler("http://portal.hitsz.edu.cn/portal",0,&wg)
 	wg.Wait()
+	fmt.Println("correct ending ")
 }
